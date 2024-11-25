55fb63ca2a1325927b8aaf85432d0bc67f9e2565
Moritz Reinhold <reinhold.moritz887@gmail.com>
2024-11-25 11:12:57 +0100

    add: const for fetch-result in main.ts

--- src/functions/fetch.ts
+++ src/functions/fetch.ts
@@ -1,7 +1,12 @@
 import { IQuestion } from "../interfaces/IQuestion";
 
 export async function fetchQuestions(url: string) {
-    const result = await fetch(url);
-    const questions: IQuestion[] = await result.json();
-    return questions;
+    try {
+        const result = await fetch(url);
+        const questions: IQuestion[] = await result.json();
+        return questions;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 }
