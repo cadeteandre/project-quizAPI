b8dfc9bfddb0e25e17635d0fbad3500b4eb3e5f4
Moritz Reinhold <reinhold.moritz887@gmail.com>
2024-11-25 11:28:26 +0100

    merge problem

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,5 @@ const questionEinfach: IQuestion[] = await fetchQuestions("https://vz-wd-24-01.g
 const questionEasy: IQuestion[] = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/easy.json");
 const questionSchwer: IQuestion[] = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/schwer.json");
 const questionHard: IQuestion[] = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/hard.json");
+
+console.log("test");
