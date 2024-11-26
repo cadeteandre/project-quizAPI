f6cbfcecf3c3bf0e1b72a884b6d9bc89bb323f24
Andre Cadete <andrecadete70@gmail.com>
2024-11-26 12:35:58 +0100

    show answers

--- src/functions/endGame.ts
+++ src/functions/endGame.ts
@@ -1,13 +1,22 @@
 import { IQuestion } from "../interfaces/IQuestion";
 
-export function endGame(correctAnswers: number, questions: IQuestion[]): void {
+export function endGame(correctAnswers: number, allQuestions: IQuestion[]): void {
     const modal = document.querySelector('#quizResultModal') as HTMLDivElement;
     const correctAnswersElement = document.querySelector('#correctAnswers') as HTMLSpanElement;
     const totalQuestionsElement = document.querySelector('#totalQuestions') as HTMLSpanElement;
+    const allAnswers = document.querySelectorAll('.used') as NodeListOf<HTMLParagraphElement>;
 
     correctAnswersElement.textContent = correctAnswers.toString();
-    totalQuestionsElement.textContent = questions.length.toString();
-    console.log(correctAnswersElement, totalQuestionsElement)
+    totalQuestionsElement.textContent = allQuestions.length.toString();
+    const rightAnswers = allQuestions.map((question) => question.answers[question.correct]);
+    
+    for(let i: number = 0; i < rightAnswers.length; i++) {
+        allAnswers.forEach((answer) => {
+            if(answer.textContent === rightAnswers[i]) {
+                answer.style.color = 'green';
+            }
+        })
+    }
 
     modal.classList.remove('hidden');
 
