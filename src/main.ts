fdf352c11a664dbb2c066f7c627b45db30a00c3c
Andre Cadete <andrecadete70@gmail.com>
2024-11-26 13:32:22 +0100

    deploy problem

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { fetchQuestions } from "./functions/fetch";
 import { startGame } from "./functions/startGame";
 import { IQuestion } from "./interfaces/IQuestion";
 
-export const questionEinfach: IQuestion[] = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/leicht.json");
-export const questionEasy: IQuestion[] = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/easy.json");
-export const questionSchwer: IQuestion[] = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/schwer.json");
-export const questionHard: IQuestion[] = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/hard.json");
+export let questionEinfach: IQuestion[] = [];
+export let questionEasy: IQuestion[] = [];
+export let questionSchwer: IQuestion[] = [];
+export let questionHard: IQuestion[] = [];
+
+async function initQuestions() {
+    questionEinfach = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/leicht.json");
+    questionEasy = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/easy.json");
+    questionSchwer = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/schwer.json");
+    questionHard = await fetchQuestions("https://vz-wd-24-01.github.io/typescript-quiz/questions/hard.json");
+}
+
+initQuestions();
 
 //* ------------------ Selecting HTML elements ------------------
 const startBtn = document.querySelector("#start-btn") as HTMLButtonElement;
